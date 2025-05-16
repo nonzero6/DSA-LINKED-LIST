66f87a317539884128bd0074774c6c6024ea327b
Nonzero <gbrkl9999@gmail.com>
2025-05-16 10:04:03 +0800

    Update final_solution.py
    tanda panah

--- final_solution.py
+++ final_solution.py
@@ -7,7 +7,7 @@ class SinglyLinkedList:
     def __init__(self):
         self.head = None
         
-def print_singly_linked_list(node, sep=' '):
+def print_singly_linked_list(node, sep=' => '):
     while node:
         print(node.data, end='')
         node = node.next
